7757bab8ce92c3a647ade42bef667806f72b0531
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2022-06-15 10:24:45 +0100

    created NewModel

---
+++ lib/data_layer/models/new/new_model.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'new_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+NewModel _$NewModelFromJson(Map<String, dynamic> json) => NewModel(
+      data: DataModel.fromJson(json['data']),
+    );
